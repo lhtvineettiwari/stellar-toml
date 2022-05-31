5dc97ce794d70998aa8cf7e86c26bbff6c794b76
vineet8010 <vineet@leewayhertz.com>
2022-05-31 16:41:54 +0530

    fixed

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
-const app = express()
+const app = express();
+const product = require("./api/product");
 const path = require('path');
 const cors = require("cors")
 app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+app.use(express.json({ extended: false }));
 app.get("/", (req, res)=>{
     res.json({
         "status":"Live"
@@ -21,7 +17,5 @@ app.get('/.well-known/stellar.toml', (req, res, next) => {
     }
     res.sendFile('stellar.toml', options);
   })
-
-app.listen(3000, ()=>{
-    console.log("Srever is running at port 3000");
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
